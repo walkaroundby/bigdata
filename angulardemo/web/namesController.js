00c8d391a89b984290cd104eaba62732e3112005
罗伟 <forestandocean@126.com>
2018-11-02 14:46:23 +0800

    初始化angular demo

---
+++ angulardemo/web/namesController.js
@@ -0,0 +1,12 @@
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+});
